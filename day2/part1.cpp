a0b1bf22f2bde24372d7632bd4a8bca9d759b882
eulegang <eulegang@eulegang.dev>
2025-12-08 20:03:09 -0600

    removing threading

--- day2/part1.cpp
+++ day2/part1.cpp
@@ -1,47 +1,24 @@
 #include <iostream>
-#include <thread>
 
+#include "chroma.h"
 #include "input.h"
 #include "model.h"
 
 using namespace std;
-
-void do_work(vector<Range> ranges, atomic<long> *result) {
-  long sum = 0;
-  for (auto range : ranges) {
-    cout << "current range: " << range << " - " << range.end - range.start
-         << endl;
-    sum += range.invalid_id_sum();
-  }
-
-  cout << "subsum of invalid ids: " << sum << endl;
-  *result += sum;
-}
 
 int main() {
   string input = read_file("day2/input.txt");
   chomp(input);
 
   auto ranges = splitT<Range>(input, ",");
-
-  vector<Range> inputs[4];
-  int i = 0;
-  for (auto range : ranges) {
-    inputs[i++].push_back(range);
-    i %= 4;
+  long sum = 0;
+  for (auto &range : ranges) {
+    cout << chroma::cyan << "current range: " << chroma::purple << range
+         << chroma::cyan << " - " << chroma::yellow << range.end - range.start
+         << chroma::clear << endl;
+    sum += range.invalid_id_sum();
   }
 
-  vector<std::thread> threads;
-  atomic<long> result;
-  i = 0;
-
-  for (auto input : inputs) {
-    threads.push_back(std::thread(do_work, inputs[i++], &result));
-  }
-
-  for (auto &thread : threads) {
-    thread.join();
-  }
-
-  cout << "sum of invalid ids: " << result << endl;
+  cout << "sum of invalid ids: " << chroma::green << sum << chroma::clear
+       << endl;
 }
